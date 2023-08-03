85f8f564e9fa4c512cfb7b9f98c08dcae215f620
DevViniciusAraujo <138336476+DevViniciusAS@users.noreply.github.com>
2023-08-03 16:26:36 -0300

    aulas Paulo Borges

---
+++ exercicios/professor-PauloBorges/aula01/script.js
@@ -0,0 +1,9 @@
+/*
+var e let podem mudar os valores
+const não podem ser alteradas os valores atribuídos.
+*/
+
+const nossaString = 'Vinícius é '
+const outraString = 'Programador'
+//console.log(nossaString + outraString)
+console.log(`Você sabia que ${nossaString} ${outraString}?`)
---
+++ exercicios/professor-PauloBorges/aula02/ex002.js
@@ -0,0 +1,13 @@
+const n1 = 6
+const n2 = 2
+const adicao = n1 + n2
+console.log(adicao)
+
+const subtracao = n1 - n2
+console.log(subtracao)
+const mult = n1 * n2
+console.log(mult)
+const divisao = n1 / n2
+console.log(divisao)
+ 
+
